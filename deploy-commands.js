3123a10bfb507e14dcc5375aaac2f5d9f0bb55a7
acapzine <119457323+acapzine@users.noreply.github.com>
2024-05-30 08:03:41 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -9,7 +9,7 @@ const commandsFolder = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
   const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
